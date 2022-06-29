6fe9f24ad5f7e0df28052e62647a1991c5ccf228
sangeethkc <keyceesangi@gmail.com>
2022-06-29 21:11:53 +0530

    table

--- resources/js/components/companies/CompaniesIndex.vue
+++ resources/js/components/companies/CompaniesIndex.vue
@@ -1,3 +1,20 @@
 <template>
     Hello World!
 </template>
+
+<script>
+import useCompanies from "../../composables/companies";
+import { onMounted } from "vue";
+
+export default {
+    setup() {
+        const { companies, getCompanies } = useCompanies()
+
+        onMounted(getCompanies)
+        
+        return {
+            companies
+        }
+    }
+}
+</script>
---
+++ resources/js/composables/companies.js
@@ -0,0 +1,15 @@
+import { ref } from 'vue'
+import axios from 'axios'
+
+export default function useCompanies(){
+    const companies = ref([])
+
+    const getCompanies = async () => {
+        let response = await axios.get('/api/companies')
+        companies.value = response.data.data;
+    }
+
+    return {
+
+    }
+}
